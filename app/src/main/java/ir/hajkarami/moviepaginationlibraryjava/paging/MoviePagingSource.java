b9f74907938748a1422ffb5f7864d0bcf41b9ce2
mshajkarami <mshajkarami.dev@gmail.com>
2025-03-29 11:14:23 +0330

    Crate MoviePagingSource and add to viewModel lifecycle

---
+++ app/src/main/java/ir/hajkarami/moviepaginationlibraryjava/paging/MoviePagingSource.java
@@ -0,0 +1,46 @@
+package ir.hajkarami.moviepaginationlibraryjava.paging;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.paging.PagingState;
+import androidx.paging.rxjava3.RxPagingSource;
+
+import java.util.List;
+
+import io.reactivex.rxjava3.core.Single;
+import io.reactivex.rxjava3.schedulers.Schedulers;
+import ir.hajkarami.moviepaginationlibraryjava.api.APIClient;
+import ir.hajkarami.moviepaginationlibraryjava.model.Movie;
+import ir.hajkarami.moviepaginationlibraryjava.model.MovieResponse;
+
+public class MoviePagingSource extends RxPagingSource<Integer, Movie> {
+
+
+    @Nullable
+    @Override
+    public Integer getRefreshKey(@NonNull PagingState<Integer, Movie> pagingState) {
+        return null;
+    }
+
+    @NonNull
+    @Override
+    public Single<LoadResult<Integer, Movie>> loadSingle(@NonNull LoadParams<Integer> loadParams) {
+        try{
+
+            int page = loadParams.getKey() != null ? loadParams.getKey():1;
+
+            return APIClient.getApiInterface()
+                    .getMoviesByPage(page)
+                    .subscribeOn(Schedulers.io())
+                    .map(MovieResponse::getMovies)
+                    .map(movies -> toLoadResult(movies, page))
+                    .onErrorReturn(LoadResult.Error::new);
+        } catch (Exception e) {
+            return Single.just(new LoadResult.Error(e));
+        }
+    }
+
+    private LoadResult<Integer, Movie> toLoadResult(List<Movie> movies, int page){
+        return new LoadResult.Page(movies,page ==1 ? null : page -1 , page+1);
+    }
+}
---
+++ app/src/main/java/ir/hajkarami/moviepaginationlibraryjava/viewmodel/MovieViewModel.java
@@ -0,0 +1,43 @@
+package ir.hajkarami.moviepaginationlibraryjava.viewmodel;
+
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelKt;
+import androidx.paging.Pager;
+import androidx.paging.PagingConfig;
+import androidx.paging.PagingData;
+import androidx.paging.rxjava3.PagingRx;
+
+import io.reactivex.rxjava3.core.Flowable;
+import ir.hajkarami.moviepaginationlibraryjava.model.Movie;
+import ir.hajkarami.moviepaginationlibraryjava.paging.MoviePagingSource;
+import kotlinx.coroutines.CoroutineScope;
+
+public class MovieViewModel extends ViewModel {
+    public Flowable<PagingData<Movie>> moviePagingDataFlowable;
+
+    public MovieViewModel(){
+        init();
+    }
+
+    private void init() {
+
+        // Define Paging Source
+        MoviePagingSource moviePagingSource = new MoviePagingSource();
+
+        Pager<Integer, Movie> pager = new Pager(
+                new PagingConfig(
+                        20,
+                        20,
+                        false,
+                        20,
+                        20*499
+                ),
+                () -> moviePagingSource);
+
+        // Flowable
+        moviePagingDataFlowable = PagingRx.getFlowable(pager);
+        CoroutineScope coroutineScope = ViewModelKt.getViewModelScope(this);
+        PagingRx.cachedIn(moviePagingDataFlowable, coroutineScope);
+
+    }
+}
