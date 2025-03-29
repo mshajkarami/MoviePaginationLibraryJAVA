5ad202aa02676090ad3f2babcaae9cc5e85ec0ce
mshajkarami <mshajkarami.dev@gmail.com>
2025-03-29 09:11:12 +0330

    adding the required library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,4 +40,36 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+    // Retrofit 2.9.0
+    val retrofitVersions = "2.9.0"
+    implementation("com.squareup.retrofit2:retrofit:$retrofitVersions")
+    implementation("com.google.code.gson:gson:$retrofitVersions")
+    implementation("com.squareup.retrofit2:converter-gson::$retrofitVersions")
+    // RxJava3 with retrofit
+    implementation("com.squareup.retrofit2:adapter-rxjava3:$retrofitVersions")
+
+    // paging Library
+    val pagingVersions = "3.1.1"
+    implementation("androidx.paging:paging-runtime:$pagingVersions")
+    // optional - RxJava3 support
+    implementation("androidx.paging:paging-rxjava3:$pagingVersions")
+
+    // Hilt Dagger
+    implementation("com.google.dagger:hilt-android:2.41")
+    annotationProcessor("com.google.dagger:hilt-compiler:2.41")
+
+    // Glide
+    implementation("com.github.bumptech.glide:glide:4.13.0")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.13.0")
+
+    // LIfeCycle
+    val lifecycleVersion = "2.5.0-alpha04"
+
+    // ViewModel
+    implementation ("androidx.lifecycle:lifecycle-viewmodel:$lifecycleVersion")
+    implementation ("androidx.lifecycle:lifecycle-extensions:2.2.0")
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycleVersion")
+
+
 }
